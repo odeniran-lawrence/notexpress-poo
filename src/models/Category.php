389f02df11c65404cb79bee9e5620503bf82b418
Jensone <hello@jensone.com>
2023-12-20 11:31:12 +0100

    Add getters and setters for Category model properties

--- src/models/Category.php
+++ src/models/Category.php
@@ -8,5 +8,33 @@ class Category extends Model
 {
     private int $id;
     private string $name;
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
 }
 // Don't write any code below this line
--- views/home.php
+++ views/home.php
@@ -1,19 +1,6 @@
 <?php 
 $pageTitle = 'Homepage';
 
-use models\Note;
-
-$note = new Note();
-$note->setTitle('My first note')
-    ->setContent('The first note')
-    ->setUserId(1)
-    ;
-
-echo $note->getTitle();
-echo $note->getContent();
-echo $note->getUserId();
-
-
 
 ?>
 
