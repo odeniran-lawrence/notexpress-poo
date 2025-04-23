cf7e7a5b9bf58c047d54688e13585e440f29e256
odeniran-lawrence <odelaw9@gmail.com>
2025-04-23 15:35:45 +0200

    changement de route

--- index.php
+++ index.php
@@ -1,39 +1,16 @@
-<?php 
+<?php
+
 
 require_once __DIR__ . '/autoloading.php';
 require_once __DIR__ . '/vendor/autoload.php';
+
+use controllers\Router;
 
 $request = $_SERVER['REQUEST_URI'];
 
 include_once __DIR__ . '/views/components/header.php';
 
 // Routes
-switch ($request) {
-    case '' :
-        require __DIR__ . '/views/home.php';
-        break;
-    case '/' :
-        require __DIR__ . '/views/home.php';
-        break;
-    case '/notes' :
-        require __DIR__ . '/views/notes/all.php';
-        break;
-    case '/note' :
-        require __DIR__ . '/views/notes/show.php';
-        break;
-    case '/note/add' :
-        require __DIR__ . '/views/notes/add.php';
-        break;
-    case '/note/edit' :
-        require __DIR__ . '/views/notes/edit.php';
-        break;
-    default:
-        http_response_code(404);
-        require __DIR__ . '/views/404.php';
-        break;
-}
+Router::route($request);
 
 include_once __DIR__ . '/views/components/footer.php';
-
-
-
